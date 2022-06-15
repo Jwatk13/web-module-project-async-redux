56812a4210422c0f2b8643f8c62c0426a5f9e3f3
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-15 17:47:13 -0400

    clean

---
+++ src/Agify.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Agify = () => {
+
+    return (
+        <h3></h3>
+    )
+}
+
+export default Agify;
---
+++ src/AgifyList.js
@@ -0,0 +1,10 @@
+// need the data to go here 
+import React from 'react';
+
+const AgifyList = props => {
+    return (
+        <div></div>
+    )
+}
+
+export default AgifyList;
