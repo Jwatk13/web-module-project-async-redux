7a7587de89271dbcba74dd6cb49d5cf64b7b93c8
Joseph Watkins <josephxwatkins@gmail.com>
2022-06-15 16:40:25 -0400

    setting up form

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Form extends React.Component {
+    render() {
+        return (
+            <>
+                <form id="agifyForm">
+                    <input
+                        
+                    />
+                </form>
+            </>
+        )
+    }
+}
